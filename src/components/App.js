a90d5b1c5d97d5418de2f0910d69566f70cb8dfb
zeeebr <zeeebr@mail.ru>
2020-02-27 17:18:47 +0500

    inital commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Article from './Article'
+import articles from '../fixtures'
+
+function App() {
+    return(
+        <div>
+        <h1>App Name</h1>
+        <Article article={articles[0]}/>
+      </div>
+    )
+}
+
+export default App
---
+++ src/components/Article.js
@@ -0,0 +1,21 @@
+import React from 'react'
+function Article(props) {
+    const {article} = props
+    const body = <section>{article.text}</section>
+    return(
+        <div className = 'hello' style={{color: 'red'}}>
+            <h2>
+                {article.title}
+                <button onClick = {handleClick}>close</button>
+            </h2>
+            {body}
+            <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
+        </div>
+    )
+}
+
+function handleClick() {
+    console.log('cliced')
+}
+
+export default Article
