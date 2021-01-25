68baa0ac35474a04beade393c78bb50e7c0a8b4c
zeeebr <zeeebr@mail.ru>
2021-01-25 16:29:11 +0500

    added 2021

--- src/components/Count.js
+++ src/components/Count.js
@@ -33,42 +33,42 @@ class Count extends Component {
                         <tbody>
                             <tr>
                                 <th>University</th>
-                                <th>2015</th>
                                 <th>2016</th>
                                 <th>2017</th>
                                 <th>2018</th>
                                 <th>2019</th>
                                 <th>2020</th>
+                                <th>2021</th>
                                 <th>ALL</th>
                             </tr>
                             <tr>
                                 <td>WoS</td>
-                                <td>{this.state.data.wos['2015']}<sup>&nbsp;{this.state.data.wos['2015+'] > 0 ? '+'+this.state.data.wos['2015+'] : ''}</sup></td>
                                 <td>{this.state.data.wos['2016']}<sup>&nbsp;{this.state.data.wos['2016+'] > 0 ? '+'+this.state.data.wos['2016+'] : ''}</sup></td>
                                 <td>{this.state.data.wos['2017']}<sup>&nbsp;{this.state.data.wos['2017+'] > 0 ? '+'+this.state.data.wos['2017+'] : ''}</sup></td>
                                 <td>{this.state.data.wos['2018']}<sup>&nbsp;{this.state.data.wos['2018+'] > 0 ? '+'+this.state.data.wos['2018+'] : ''}</sup></td>
                                 <td>{this.state.data.wos['2019']}<sup>&nbsp;{this.state.data.wos['2019+'] > 0 ? '+'+this.state.data.wos['2019+'] : ''}</sup></td>
                                 <td>{this.state.data.wos['2020']}<sup>&nbsp;{this.state.data.wos['2020+'] > 0 ? '+'+this.state.data.wos['2020+'] : ''}</sup></td>
+                                <td>{this.state.data.wos['2021']}<sup>&nbsp;{this.state.data.wos['2021+'] > 0 ? '+'+this.state.data.wos['2021+'] : ''}</sup></td>
                                 <td>{this.state.data.wos['all']}</td>
                             </tr>
                             <tr>
                                 <td>Scopus</td>
-                                <td>{this.state.data.scopus['2015']}<sup>&nbsp;{this.state.data.scopus['2015+'] > 0 ? '+'+this.state.data.scopus['2015+'] : ''}</sup></td>
                                 <td>{this.state.data.scopus['2016']}<sup>&nbsp;{this.state.data.scopus['2016+'] > 0 ? '+'+this.state.data.scopus['2016+'] : ''}</sup></td>
                                 <td>{this.state.data.scopus['2017']}<sup>&nbsp;{this.state.data.scopus['2017+'] > 0 ? '+'+this.state.data.scopus['2017+'] : ''}</sup></td>
                                 <td>{this.state.data.scopus['2018']}<sup>&nbsp;{this.state.data.scopus['2018+'] > 0 ? '+'+this.state.data.scopus['2018+'] : ''}</sup></td>
                                 <td>{this.state.data.scopus['2019']}<sup>&nbsp;{this.state.data.scopus['2019+'] > 0 ? '+'+this.state.data.scopus['2019+'] : ''}</sup></td>
                                 <td>{this.state.data.scopus['2020']}<sup>&nbsp;{this.state.data.scopus['2020+'] > 0 ? '+'+this.state.data.scopus['2020+'] : ''}</sup></td>
+                                <td>{this.state.data.scopus['2021']}<sup>&nbsp;{this.state.data.scopus['2021+'] > 0 ? '+'+this.state.data.scopus['2021+'] : ''}</sup></td>
                                 <td>{this.state.data.scopus['all']}</td>
                             </tr>
                             <tr>
                                 <td>All in year</td>
-                                <td>{this.state.data.all['2015']}</td>
                                 <td>{this.state.data.all['2016']}</td>
                                 <td>{this.state.data.all['2017']}</td>
                                 <td>{this.state.data.all['2018']}</td>
                                 <td>{this.state.data.all['2019']}</td>
                                 <td>{this.state.data.all['2020']}</td>
+                                <td>{this.state.data.all['2021']}</td>
                                 <td><b>{this.state.data.all['all']}</b><sup>&nbsp;{this.state.data.all['all+'] > 0 ? '+'+this.state.data.all['all+'] : ''}</sup></td>
                             </tr>
                         </tbody>
